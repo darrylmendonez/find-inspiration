3084cd53378ff7c2ceb89835b9180a44d37e3c9b
Darryl.Mendonez <darryl.mendonez@Laptop.home>
2020-06-18 00:50:29 -0400

    Updates

---
+++ precache-manifest.3ea48412f1f1a34bcf0aef6a7b12aada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b967a221476cab3a86edd3e998edee",
+    "url": "/find-inspiration/index.html"
+  },
+  {
+    "revision": "8a77ac4cfe4fa61d548e",
+    "url": "/find-inspiration/static/css/main.73fe9547.chunk.css"
+  },
+  {
+    "revision": "572fb5e9d3d0124fec90",
+    "url": "/find-inspiration/static/js/2.4479effc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/find-inspiration/static/js/2.4479effc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a77ac4cfe4fa61d548e",
+    "url": "/find-inspiration/static/js/main.759414d2.chunk.js"
+  },
+  {
+    "revision": "07e711c57569690e6781",
+    "url": "/find-inspiration/static/js/runtime-main.f80ff1be.js"
+  }
+]);
